c0bbc8fde87412589c52e902a41532e3fdd0e5d0
fbmnds <fbmnds7@gmail.com>
2012-11-03 20:50:06 +0100

    various functions added

--- src/latin_squares/core.clj
+++ src/latin_squares/core.clj
@@ -62,21 +62,72 @@
                        :else (conj y (nth x row)))
                  (inc row)))))
 
-(defn contains-:nil?
-  [x]
-  (cond (= (some #{:nil} x) :nil) true :else false))
-
-(defn part-rows
+(defn part-rows-1
   [x dim]
   (assert (= (max-row-len x) (min-row-len x)))
-  (assert (< dim (count x)))
-  (assert (< dim (max-row-len x)))
+  (assert (<= dim (count x)))
+  (assert (<= dim (max-row-len x)))
   (loop [y []
          row 0]
     (cond (= row (count x)) y
           :else (recur
                  (conj y (vec (partition dim 1 (take dim (repeat :nil)) (nth x row))))
                  (inc row)))))
+
+(defn part-rows
+  [x dim] ; dim counted from 0
+  (assert (= (max-row-len x) (min-row-len x)))
+  (assert (<= 2 dim (count x)))
+  (assert (<= 2 dim (max-row-len x)))
+  (mapv #( vec (partition dim 1 (take dim (repeat :nil)) %)) x))
+
+
+(defn square-at
+  [parted-x dim i j]
+  (assert (= dim (count (nth (nth parted-x 0) 0))))
+  (assert (<= 2 dim (count (nth parted-x 0))))
+  (assert (<= 2 dim (count parted-x)))
+  (loop [y []
+         di 0]
+    (cond (= di dim) y
+          :else (recur (conj y (nth (nth parted-x (+ i di)) j)) (inc di)))))
+
+
+(defn contains-:nil?
+  [x]
+  (cond (= (some #{:nil} (flatten x)) :nil) true :else false))
+
+
+;user> (mapv concat (map vector [1 2 3]) (map vector [4 5 6]))
+;[(1 4) (2 5) (3 6)]
+;user> (partition 3 (interleave [1 2 3 4 5] [6 7 8 9 10] [5 4 3 2 1]))
+;((1 6 5) (2 7 4) (3 8 3) (4 9 2) (5 10 1))
+;user> (transpose [[1 2 3 4 5] [6 7 8 9 10] [5 4 3 2 1]])
+;[(1 6 5) (2 7 4) (3 8 3) (4 9 2) (5 10 1)]
+(defn transpose
+  [m]
+  (loop [y (map vector (nth m 0))
+         i 1]
+    (cond (= i (count m)) y
+          :else (recur (mapv concat y (map vector (nth m i))) (inc i)))))
+
+(defn max-row-member [x] (reduce max (map #(count (set %)) x)))
+(defn min-row-member [x] (reduce min (map #(count (set %)) x)))
+
+(defn latin-square?
+  [s]
+  (assert (= (count s) (max-row-len s)))
+  (assert (= (min-row-len s) (max-row-len s)))
+  (cond (contains-:nil? s) false
+        (not= (count (set (flatten s))) (max-row-len s)) false
+        (not= (count (set s)) (max-row-len s)) false
+        (not= (count (set (transpose s))) (max-row-len s)) false
+        (not= (min-row-member s) (max-row-member s)) false
+        (not= (min-row-member (transpose s)) (max-row-member (transpose s))) false
+        (< (min-row-member s) (max-row-len s)) false
+        (< (min-row-member (transpose s)) (max-row-len s)) false
+        :else true))
+
 
 (defn mark-:nil-in-col
   [x]
